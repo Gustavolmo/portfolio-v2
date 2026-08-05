48208ddd26f209938b4ff0868d2371c040f03135
Gustavo L.M. de Oliveira <67520957+Gustavolmo@users.noreply.github.com>
2026-08-05 14:29:04 +0200

    use base path (#3)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: { outDir: 'docs' },
-  base: '/welcome/',
+  base: '/',
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
