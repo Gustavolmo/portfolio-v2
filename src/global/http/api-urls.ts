29943aa0cdb36c240236edf5a90462cd9e6c4be4
Gustavo L.M. de Oliveira <67520957+Gustavolmo@users.noreply.github.com>
2026-08-05 23:13:46 +0200

    fix prod url (#10)

--- src/global/http/api-urls.ts
+++ src/global/http/api-urls.ts
@@ -1,6 +1,6 @@
 class ApiUrls {
   private readonly origin =
-    window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'http://localhost:8080' // FIND ME - fix to prod URL when ready
+    window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://portfolio-v2-api-9kpk.onrender.com'
 
   readonly health = `${this.origin}/health`
   readonly observe = `${this.origin}/observe`
